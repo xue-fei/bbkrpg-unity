df31bb69026c8a56f3dc06a68e0dffdcddf21706
xue-fei <awnuxcvbn@163.com>
2026-03-06 16:36:41 +0800

    屏幕宽高和白色

--- Assets/Scripts/BBKRPGSimulator/Core/Definitions/Constants.cs
+++ Assets/Scripts/BBKRPGSimulator/Core/Definitions/Constants.cs
@@ -9,17 +9,17 @@ namespace BBKRPGSimulator
 
         public const int COLOR_BLACK = -16777216;
         public const int COLOR_TRANSP = 0;
-        public const int COLOR_WHITE = -3356691;
+        public const int COLOR_WHITE = -1;
 
         /// <summary>
         /// 屏幕高度 96的倍数
         /// </summary>
-        public const int SCREEN_HEIGHT = 192;
+        public const int SCREEN_HEIGHT = 96;
 
         /// <summary>
         /// 屏幕宽度 160的倍数
         /// </summary>
-        public const int SCREEN_WIDTH = 320;
+        public const int SCREEN_WIDTH = 160;
 
         public const int HEIGHT = 96;
         public const int WIDTH = 160; 
